94892b925556a390f1ea1ca74f6eaaf1c44df3db
astribli <andrewjstribling@gmail.com>
2020-08-21 19:06:37 -0500

    updated shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -196,21 +196,20 @@ alert("Thank you for playing Robot Gladiators! Come back soon!");
 var shop = function() {
   // ask player what they'd like to do
   var shopOptionPrompt = prompt(
-    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
   );
 
+  shopOptionPrompt = parseInt(shopOptionPrompt);
+
   // use switch to carry out action
-switch (shopOptionPrompt) {
-  case "REFILL":
-  case "refill":
+  switch (shopOptionPrompt) {
+  case 1:
     playerInfo.refillHealth();
     break;
-  case "UPGRADE":  
-  case "upgrade":
+  case 2:
     playerInfo.upgradeAttack();
     break;
-  case "LEAVE":  
-  case "leave":
+  case 3:
     alert("Leaving the store.");
 
     // do nothing, so function will end
